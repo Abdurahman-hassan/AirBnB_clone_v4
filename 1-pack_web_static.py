b8309a1dd65522941d44a51865460f6f5b156fd5
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-08 03:47:16 +0200

    change the function

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,36 +1,21 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo"""
+"""
+Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo
+"""
 
-import os
 from datetime import datetime
-
 from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
+    """generates a tgz archive"""
     try:
-        # Create the 'versions' directory if it doesn't exist
-        if not os.path.exists("versions"):
-            os.makedirs("versions")
-
-        # Generate a timestamped archive name
-        now = datetime.now()  # Current date and time
-        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second)
-
-        # The folder to archive
-        folder_to_archive = "web_static"
-
-        # The target archive path
-        archive_path = "versions/{}".format(archive_name)
-
-        # Create the archive using tar command
-        command = "tar -cvzf {} {}".format(archive_path, folder_to_archive)
-        print("Packing {} to {}".format(folder_to_archive, archive_path))
-
-        # Execute the command locally
-        local(command)
-
-        return archive_path
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
     except:
         return None
