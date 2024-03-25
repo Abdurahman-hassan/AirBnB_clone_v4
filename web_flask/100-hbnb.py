b1eafd3b67726c2f9dd37cc022b45cad5b0f7c51
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-25 02:24:19 +0200

    a script that starts a Flask web application:
    Your web application must be listening on 0.0.0.0, port 5000
    You must use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
    To load all cities of a State:
    If your storage engine is DBStorage, you must use cities relationship
    Otherwise, use the public getter method cities
    After each request you must remove the current SQLAlchemy Session:
    Declare a method to handle @app.teardown_appcontext
    Call in this method storage.close()
    Routes:
    /hbnb: display a HTML page like 8-index.html, done during the 0x01. AirBnB clone - Web static project
    Copy files 3-footer.css, 3-header.css, 4-common.css, 6-filters.css and 8-places.css from web_static/styles/ to the folder web_flask/static/styles
    Copy all files from web_static/images/ to the folder web_flask/static/images
    Update .popover class in 6-filters.css to enable scrolling in the popover and set max height to 300 pixels.
    Update 8-places.css to always have the price by night on the top right of each place element, and the name correctly aligned and visible (i.e. screenshots below)
    Use 8-index.html content as source code for the template 100-hbnb.html:
    Replace the content of the H4 tag under each filter title (H3 States and H3 Amenities) by &nbsp;
    Make sure all HTML tags from objects are correctly used (example: <BR /> must generate a new line)
    State, City, Amenity and Place objects must be loaded from DBStorage and sorted by name (A->Z)
    You must use the option strict_slashes=False in your route definition
    Import this 100-dump to have some data

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,47 +1,38 @@
 #!/usr/bin/python3
+
 """
-Flask App that integrates with AirBnB static HTML Template
+Starts a Flask web application
 """
+
 from flask import Flask, render_template
 
 from models import storage
 
-# flask setup
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-port = 5000
-host = '0.0.0.0'
 
 
-# begin flask page rendering
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Route to display HBNB data.
+    """
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    places = storage.all("Place")
+
+    return render_template(
+        "100-hbnb.html",
+        states=states,
+        amenities=amenities,
+        places=places
+    )
+
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    after each request, this method calls .close() (i.e. .remove()) on
-    the current SQLAlchemy Session
-    """
+def teardown(exc):
+    """Teardown function to close the data storage session."""
     storage.close()
 
 
-@app.route('/hbnb')
-def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
-    state_objs = storage.all('State').values()
-    states = dict([state.name, state] for state in state_objs)
-    amens = storage.all('Amenity').values()
-    places = storage.all('Place').values()
-    users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
-                 for user in storage.all('User').values())
-    return render_template('100-hbnb.html',
-                           states=states,
-                           amens=amens,
-                           places=places,
-                           users=users)
-
-
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=5000)
